36de01b7f279f904689e177ee7c301ac55a97f37
Yoan Malaver <yoanmalaver@gmail.com>
2021-05-26 12:17:48 -0500

    remove Link

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import MapContainer from '../components/MapContainer';
 import Informacion from '../components/Informacion';
 import Register from '../components/Register';
